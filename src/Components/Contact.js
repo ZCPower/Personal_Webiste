e851b06d8d2d7e0334e55dadee2bcda36b9b67c8
ZCPower <zachpowerxc@gmail.com>
2022-06-16 01:51:38 -0500

    modified contact, added links to portfolio links and some styling

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,8 +4,10 @@ import './Contact.css'
 function Contact() {
   return (
     <div className='contactContainer'>
-      <h2>Contact</h2>
-
+      <div>
+        <h2>Contact</h2>
+        <h4>Interested in my work? Feel free to shoot me a message!</h4>
+      </div>
       <form className='contactForm'>
         <div>
           <input placeholder='Your Name'></input>
@@ -18,7 +20,7 @@ function Contact() {
 
         <a href='https://www.linkedin.com/in/zachary-power-1418181b8/' target='_blank'><img src='https://img.icons8.com/fluency/344/linkedin-circled.png'></img></a>
         <img src='https://img.icons8.com/color/344/gmail-new.png'></img>
-        <img src='https://assets-global.website-files.com/5ec7d9f13fc8c0ec8a4c6b26/6092b794e0419d97d9b06e2b_Favicon%20256.png'></img>
+        <a href='https://www.upwork.com/freelancers/~017372d05a990f2645'><img src='https://assets-global.website-files.com/5ec7d9f13fc8c0ec8a4c6b26/6092b794e0419d97d9b06e2b_Favicon%20256.png'></img></a>
       </div>
     </div>
   )
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,13 +7,45 @@ function Projects() {
         <div className='projectContainer'>
             <h2>Projects</h2>
             <div className='project'>
-                <h3>Stranger's Things</h3>
-                <p>Craiglist Like App</p>
-                <p>Tic Tac Toe</p>
-                <p>Fitness Tracker</p>
-                <p>Grace Shopper</p>
+                <div className='projectBlurb'>
+                    <h3>Grace Shopper... TBD</h3>
+                    <p>An Ecommerce Page built from the ground up. A fullstack app.</p>
+                </div>
+                <img></img>
             </div>
-            <img src='https://img.icons8.com/ios-glyphs/344/github.png'></img>
+
+            <div className='project'>
+                <div className='projectBlurb'>
+                    <h3>Fitness Tracker... TBD</h3>
+                    <p>A fullstack web app that tracks fitness wooooooo-eeeeee!</p>
+                </div>
+                <img></img>
+            </div>
+            <div className='project'>
+                <div className='projectBlurb'>
+                    <a href='https://earnest-horse-ab4333.netlify.app/register' target='_blank'><h3>Stranger's Things</h3></a>
+                    <p>A Craiglist-Lite App built using React</p>
+                </div>
+                <img></img>
+            </div>
+            <div className='project'>
+                <div className='projectBlurb'>
+                    <a href='https://lambent-chebakia-68ed4c.netlify.app/'><h3>Art Collector</h3></a>
+                    <p>An App built to practice API calls and React. Browse the Harvard Art Museum from the comfort of your home!</p>
+                </div>
+                <img></img>
+            </div>
+            <div className='project'>
+                <div className='projectBlurb'>
+                    <a href='https://funny-donut-346f04.netlify.app/' target='_blank'><h3>Tic-Tac-Toe</h3></a>
+                    <p>A Classic Game of X's and O's</p>
+                </div>
+                <img src='https://screenshot-proxy.netlify.app/f_avif,w_336/…0c806f3de/screenshot_2022-04-09-17-42-16-0000.png'></img>
+            </div>
+            <div>
+                <a href='https://www.github.com/zcpower' target="_blank"><img src='https://img.icons8.com/ios-glyphs/344/github.png'></img></a>
+                <a href='https://www.codewars.com/users/ZachPowerXC' target='_blank'><img src='https://iceicery.github.io/pinwei/static/media/codewar.9248b102.jpg'></img></a>
+            </div>
         </div>
     )
 }
